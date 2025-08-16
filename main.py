29ac62c3c29a12c41535da51cb2c77a8e9962571
GreatLoch <1320911762@qq.com>
2025-08-16 15:41:57 +0800

    用flask 做接口

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import yaml
 import EmailPush
 import WechatBotPush
+from flask import Flask, request, jsonify
+app = Flask(__name__)
 with open('config.yaml','r',encoding='UTF-8') as config:
      config= yaml.load(config,Loader=yaml.FullLoader)
-data={'email':'1320911762@qq.com','content':'测试短信'}
-data2={
-    "msgtype": "text",
-    "text": {
-                "content": "机房大门打开"
-        }
-}
-print(config)
-EmailPush.WYsend_email(data,config['WYEmail'])
-WechatBotPush.PushBot(data2,config['WechatBot'])
+@app.route('/send/wyemail',methods=['POST'])
+def WyEmailPush():
+    data=request.get_json()
+    try:
+        response=EmailPush.WYsend_email(data,config['WYEmail'])
+        return    jsonify({"code": 200, "message": "发送成功"})
+    except Exception as e:
+        return jsonify({"code": 500, "message":e})
+@app.route('/send/wechatbot',methods=['POST'])
+def WechatBot():
+    data=request.get_json()
+    try:
+        response=WechatBotPush.PushBot(data,config['WechatBot'])
+        return    jsonify({"code": 200, "message": "发送成功"})
+    except Exception as e:
+        return jsonify({"code": 500, "message":e})
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=1992)
